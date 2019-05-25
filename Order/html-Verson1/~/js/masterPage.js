d46c21d45d3574fee0faf8a41dd5e8b3925c02fd
于 悠悠 <823yoga@gmail.com>
2019-05-25 22:53:28 +0800

    購物車加入 [份量]

--- Order/html-Verson1/~/js/masterPage.js
+++ Order/html-Verson1/~/js/masterPage.js
@@ -23,4 +23,53 @@
                     $("#rwdMenu").click();
                 });
             }
-        });
+});
+
+//購物車---------------------------------------
+//$(function () {
+//    main();
+//});
+
+main();
+
+function main() {
+
+    ShowShopCart();
+    NoItemCart();
+}
+
+//顯示 購物車
+function ShowShopCart() {
+    //滑出 購物車
+    $('#navShopCart').hover(function () {
+        $('.shopCart').show('slow');
+    });
+
+    $('.shopCart').hover(function () {
+        //滑鼠移入事件
+
+    }, function () {
+        //滑鼠移出事件--------
+
+        //隱藏購物車
+        $('.shopCart').hide();
+
+    });
+
+}
+
+//無商品的購物車--------------------------------
+function NoItemCart() {
+    var cartCount = document.getElementsByClassName("shopCartItem");
+
+    if (cartCount.length <= 0) {
+        //購物車無商品
+        $('.noItem').show();
+        $('.shopCartFooter').hide();
+    }
+    else {
+        //購物車有商品
+        $('.noItem').hide();
+        $('.shopCartFooter').show();
+    }
+}

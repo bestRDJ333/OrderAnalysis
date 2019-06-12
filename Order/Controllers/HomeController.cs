171fa25904b6f322d2395319a4e84a37ac4a0f9d
豈几 羊羽 言身寸 <shadow_kai@hotmail.com.tw>
2019-06-12 18:32:37 +0800

    bug fix

--- Order/Controllers/HomeController.cs
+++ Order/Controllers/HomeController.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Order.Models;
 namespace Order.Controllers
 {
     public class HomeController : Controller
     {
+        ShopCart sc = new ShopCart();
         // GET: Home
         public ActionResult Index()
         {
+            int mID = 4;
+            ViewBag.sumPrice = sc.SumTotal(mID);
+            ViewBag.itemAmt = sc.GetCartItem(mID).Count();
+            TempData["ShopCart"] = sc.GetCartItem(mID);
             return View();
         }
 
@@ -20,5 +26,5 @@ namespace Order.Controllers
             return View("Index");
         }
     }
-    
+
 }

3db9600b59eca65a458159850e498247f9bec5e9
Kai <shadowkai0121@gmail.com>
2019-06-13 13:35:35 +0800

    fixe product modal

--- Order/Controllers/ShopController.cs
+++ Order/Controllers/ShopController.cs
@@ -12,17 +12,15 @@ namespace Order.Controllers
     // todo: 會員ID傳遞實裝
     // todo: CheckOut頁面修改數量後ShopCart同步更新
     // todo: 結帳點選確認才送出post
-    // todo: 主菜以外的產品卡與Modal無法對應
-    // todo: 詳細的debug
 
     public class ShopController : Controller
     {
         SMIT09Entities db = new SMIT09Entities();
         ShopCart sc = new ShopCart();
+        static int mID = 4;
         // GET: Menu
         public ActionResult Menu()
         {
-            int mID = 4;
             TempData["ShopCart"] = sc.GetCartItem(mID);
             ViewBag.sumPrice = sc.SumTotal(mID);
             ViewBag.itemAmt = sc.GetCartItem(mID).Count();
@@ -32,7 +30,6 @@ namespace Order.Controllers
         // GET: Product
         public ActionResult Product()
         {
-            int mID = 4;
             // var mID = (Session["who"] as Member).MemberID;
             var product = db.Products.ToList();
 
@@ -48,7 +45,6 @@ namespace Order.Controllers
         // GET: AddCart
         public ActionResult addCart(int pID, int? amt)
         {
-            int mID = 4;
             // 取得會員ID
             //int mID = (Session["who"] as Member).MemberID;          
 
@@ -60,7 +56,6 @@ namespace Order.Controllers
         // GET: DelCart
         public ActionResult DelCart(int pID)
         {
-            int mID = 4;
             //int mID = (Session["who"] as Member).MemberID;    
             sc.DelItem(pID, mID);
             return RedirectToAction("Product");
@@ -69,7 +64,6 @@ namespace Order.Controllers
         // GET: CheckOut
         public ActionResult CheckOut()
         {
-            int mID = 4;
             TempData["ShopCart"] = sc.GetCartItem(mID);
             ViewBag.itemAmt = sc.GetCartItem(mID).Count();
             ViewBag.sumPrice = sc.SumTotal(mID);
@@ -79,7 +73,6 @@ namespace Order.Controllers
         [HttpPost]
         public ActionResult Confirm(int totalPrice, string ReceiverName, string ReceiverPhone, string ReceiverAddress)
         {
-            int mID = 4;
             sc.ConfirmOrder(mID, totalPrice, ReceiverName, ReceiverPhone, ReceiverAddress);
 
             return RedirectToAction("Product");
